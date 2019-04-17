916df47da008a4714491fef9347cb7991e41a50b
melitus <asmelitus@gmail.com>
2019-04-17 14:28:10 +0300

    created mongoose config

---
+++ server/src/grapgql/types/user/index.js
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-server'
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    name: String
+    email: String
+    picture: String
+  }
+  type Query {
+    me: User
+  }
+`
+
+export default typeDefs
