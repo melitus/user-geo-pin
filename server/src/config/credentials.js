defe79328894893438d2bc9c581186e1a1c21732
melitus <asmelitus@gmail.com>
2019-04-17 14:32:47 +0300

    created the credentials config file

---
+++ server/src/config/credentials.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+// import .env variables
+require('dotenv-safe').load({
+  path: path.join(__dirname, '../../.env'),
+  sample: path.join(__dirname, '../../.env.example'),
+  allowEmptyValues: true
+})
+
+module.exports = {
+  appKey: {
+    name: process.env.APP_NAME,
+    port: process.env.APP_PORT || 8000,
+    host: process.env.HOST || 'localhost',
+    env: process.env.NODE_ENV
+  },
+  mongo: {
+    uri:
+      process.env.NODE_ENV === 'test'
+        ? process.env.MONGO_URI_TESTS
+        : process.env.MONGO_URI
+  },
+  appLog: {
+    logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev'
+  }
+}
